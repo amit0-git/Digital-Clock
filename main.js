6c0eeb7a808d622d4beaac47a7e4329f43ccc569
Amit <root.avanti@gmail.com>
2022-06-30 23:34:48 +0530

    changed cookie storage to local storage

--- main.js
+++ main.js
@@ -1,84 +1,82 @@
-const date1=new Date();
-var hour1=date1.getHours();
-var num=0;
-var colors=["rgb(10, 120, 10)","rgb(20, 120, 20)","rgb(30, 120, 30)","rgb(40, 120, 40)","rgb(50, 120, 50)","rgb(60, 120, 60)","rgb(70, 120, 70)","rgb(80, 120, 80)","rgb(90, 120, 90)","rgb(100, 120, 100)","rgb(110, 120, 110)","rgb(120, 120, 120)","rgb(130, 120, 130)","rgb(140, 120, 140)","rgb(150, 120, 150)","rgb(160, 120, 160)","rgb(170, 120, 170)","rgb(180, 120, 180)","rgb(190, 120, 190)","rgb(200, 120, 200)","rgb(210, 120, 210)","rgb(220, 120, 220)","rgb(230, 120, 230)","rgb(240, 120, 240)","rgb(250, 120, 250)"]
-function displayDate(){
-    
-    const date=new Date();
- 
-    var hrs=document.getElementById("hrs");
-    var min=document.getElementById("min");
-    var sec=document.getElementById("sec");
-    
-    document.title=date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
-    var hour=date.getHours();
-    hrs.innerHTML=hour;
-    min.innerHTML=date.getMinutes();
-    sec.innerHTML=date.getSeconds();
-    
-    var secc=document.getElementById("sec");
-    secc.style.background=colors[num];
-    
-    num+=1;
-    
-    if (num>24){
-        num=0;
+const date1 = new Date();
+var hour1 = date1.getHours();
+var num = 0;
+var colors = ["rgb(10, 120, 10)", "rgb(20, 120, 20)", "rgb(30, 120, 30)", "rgb(40, 120, 40)", "rgb(50, 120, 50)", "rgb(60, 120, 60)", "rgb(70, 120, 70)", "rgb(80, 120, 80)", "rgb(90, 120, 90)", "rgb(100, 120, 100)", "rgb(110, 120, 110)", "rgb(120, 120, 120)", "rgb(130, 120, 130)", "rgb(140, 120, 140)", "rgb(150, 120, 150)", "rgb(160, 120, 160)", "rgb(170, 120, 170)", "rgb(180, 120, 180)", "rgb(190, 120, 190)", "rgb(200, 120, 200)", "rgb(210, 120, 210)", "rgb(220, 120, 220)", "rgb(230, 120, 230)", "rgb(240, 120, 240)", "rgb(250, 120, 250)"]
+function displayDate() {
+    //function for displaying date
+    const date = new Date();
+
+    var hrs = document.getElementById("hrs");
+    var min = document.getElementById("min");
+    var sec = document.getElementById("sec");
+
+    document.title = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    var hour = date.getHours();
+    hrs.innerHTML = hour;
+    min.innerHTML = date.getMinutes();
+    sec.innerHTML = date.getSeconds();
+
+    var secc = document.getElementById("sec");
+    secc.style.background = colors[num];
+
+    num += 1;
+
+    if (num > 24) {
+        num = 0;
     }
- 
+
 
 }
 
-
-function getname(name){
-    var nn=name.split("=")[1];
-    console.log(nn);
-    return nn;
-
-}
-function promptbox(){
-    
-    
+function promptbox() {
+    //function for asking name from the user
     var person = prompt("Please enter your name", "Amit Verma");
     var name;
-   
+
     if (person == null || person == "") {
-        
-    name = "";
+
+        name = "Sir!";
+
     }
-     else {
+    else {
         name = person;
-        document.cookie="name="+name;
-        console.log(document.cookie);
-       
+
+        localStorage.setItem("name", name);//store the name in local storage
+        console.log(localStorage.getItem("name"));
+
 
     }
     location.reload();//reload the document after assigning the new name
 }
 
-function greetbox(){
-   
-    if (getname(document.cookie)){
-        wish(getname(document.cookie),hour1);
+function greetbox() {
+    //function if the name is stored is  stored in local storage then display it other wise ask for name
+    if (localStorage.getItem("name")) {
+        wish(localStorage.getItem("name"), hour1);
 
     }
-   
-    
-   
-   
+    else {
+        promptbox();
+    }
+
+
+
+
 
 }
 
-function wish(name,hour){
-    var greetdiv=document.getElementById("greet");
-    if (hour<12){
-        greetdiv.innerHTML="Good Morning "+name+" !";
+function wish(name, hour) {
+    //wish the user according to time
+    var greetdiv = document.getElementById("greet");
+    if (hour < 12) {
+        greetdiv.innerHTML = "Good Morning " + name + " !";
     }
 
-    else if(hour<18){
-        greetdiv.innerHTML="Good Afternoon "+name+" !";
+    else if (hour < 18) {
+        greetdiv.innerHTML = "Good Afternoon " + name + " !";
     }
-    else{
-        greetdiv.innerHTML="Good Evening "+name+" !";
+    else {
+        greetdiv.innerHTML = "Good Evening " + name + " !";
     }
 
 }
@@ -87,6 +85,6 @@ greetbox();
 
 
 
-setInterval(displayDate,1000);
-    
+setInterval(displayDate, 1000);
+
 
